8bec1051dc5e888571dde74d66976d2cc460030c
4NG3LD4NIEL <aromero_21@alu.uabcs.mx>
2024-11-19 11:09:52 -0700

    rutas,act7

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,31 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/Saludar/{name}/{lastname?}', function ($name,$lastname = "Doe") {
+    return 'welcome '. $name .' '. $lastname;
+})->where(['name' => '^[a-zA-Z]+$', 'lastname' => '^[a-zA-Z]+$']);
+
+Route::get('/sumar/{n1}/{n2}', function ($n1,$n2) {
+    $resultado = $n1 + $n2;
+    return 'Resultado '. $resultado;
+})->where(['n1' => '^[0-9]+$','n2' => '^[0-9]+$']);
+
+Route::get('/restar/{n1}/{n2}', function ($n1,$n2) {
+    $resultado = $n1 - $n2;
+    return 'Resultado '. $resultado;
+})->where(['n1' => '^[0-9]+$','n2' => '^[0-9]+$']);
+
+Route::get('/multi/{n1}/{n2}', function ($n1,$n2) {
+    $resultado = $n1 * $n2;
+    return 'Resultado '. $resultado;
+})->where(['n1' => '^[0-9]+$','n2' => '^[0-9]+$']);
+
+Route::get('/dividir/{n1}/{n2}', function ($n1,$n2) {
+    $resultado = $n1 / $n2;
+    return 'Resultado '. $resultado;
+})->where(['n1' => '^[0-9]+$','n2' => '^[0-9]+$']);
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
